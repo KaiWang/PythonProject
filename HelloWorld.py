c8b8d3a4a22feab2839f666c34c0e09d851e7b7a
ryan <fswklotto@gmail.com>
2020-04-01 12:58:37 -0400

    learning string functions

--- HelloWorld.py
+++ HelloWorld.py
@@ -98,3 +98,112 @@ print(len('hello Mr. Bean!!'))
 print("Hello World")
 print(len("ploopy"))
 print('hello \nRyan')
+print(2+4)
+print(999999999999999999999999*9999999999999999999)
+print('Hello word')
+print(66/11)
+print((4 + 7)*6)
+print((60+20)*2-60)
+print(1 / 2)
+print("proffesuer pippy pee-pee diarstain poopypants")
+print('Dr diaper')
+my_dogs = 2
+print(my_dogs)
+my_dogs = 6
+print(my_dogs)
+a = 5
+print(a)
+a = 'hello'
+print(a)
+a = 6
+print(a + a)
+a = a + a + a
+print(a)
+print(type(a))
+print(type(30.1))
+print(type('Hello'))
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
+print('print')
+print("hello word")
+print('this code is also a string code')
+print("I'm having a nice day. Are you having a nice day?")
+print("Hello world 1")
+print("Hello world 2")
+print("Hello world 3")
+print('hello \nword')
+print("        \n     ")
+print('hello \n    word')
+print('hello \n    Ryan')
+print('   \n    \n     \n     \n     \n     \n     \n     \n     \n     \n     \n     ')
+print('hello \tRyan')
+print('hello \twei')
+print('hello \tkai')
+print(len('hello'))
+a = 'hello'
+print(len(a))
+print(len('hello Mr. Bean!!'))
+print("Hello World")
+print(len("ploopy"))
+print('hello \nRyan')
+print(2+4)
+print(999999999999999999999999*9999999999999999999)
+print('Hello word')
+print(66/11)
+print((4 + 7)*6)
+print((60+20)*2-60)
+print(1 / 2)
+print("proffesuer pippy pee-pee diarstain poopypants")
+print('Dr diaper')
+my_dogs = 2
+print(my_dogs)
+my_dogs = 6
+print(my_dogs)
+a = 5
+print(a)
+a = 'hello'
+print(a)
+a = 6
+print(a + a)
+a = a + a + a
+print(a)
+print(type(a))
+print(type(30.1))
+print(type('Hello'))
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
+print('print')
+print("hello word")
+print('this code is also a string code')
+print("I'm having a nice day. Are you having a nice day?")
+print("Hello world 1")
+print("Hello world 2")
+print("Hello world 3")
+print('hello \nword')
+print("        \n     ")
+print('hello \n    word')
+print('hello \n    Ryan')
+print('   \n    \n     \n     \n     \n     \n     \n     \n     \n     ')
+print('hello \tRyan')
+print('hello \twei')
+print('hello \tkai')
+print(len('hello'))
+a = 'hello'
+print(len(a))
+print(len('hello Mr. Bean!!'))
+print("Hello World")
+print(len("ploopy"))
+print('hello \nRyan'
+      )
+print(len("ploopy"))
+
+
+
+
+
+
+
